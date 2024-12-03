1ec127f86518dfacdbbb1a44d2f665611fa8e8dc
lester <lester.achondo@amellar.com>
2024-12-03 13:36:08 +0800

    Change the page title name and logo.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <center>      
         <h1>Hello Guys, I'm Lester</h1>
       </center>
