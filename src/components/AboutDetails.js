e3f00efd2213d5f6d6f7db3bbe20a4b06d9076d1
lester <lester.achondo@amellar.com>
2024-12-09 18:50:09 +0800

    Change some content of about component.

--- src/components/AboutDetails.js
+++ src/components/AboutDetails.js
@@ -7,17 +7,20 @@ const AboutDetails = () => {
       <div className='aboutContent'>
         <h2>So, who am I?</h2>
         <p>
-          I’m Lester, a full-stack web developer with expertise in both front-end and back-end development. My career began when I earned a Bachelor of Science in Computer Science from the University of Caloocan City, where I gained skills in both hardware and software.        </p>
+          I’m Lester, a full-stack web developer with expertise in both front-end and back-end development. My career began when I earned a Bachelor of Science in Computer Science from the University of Caloocan City, where I gained skills in both hardware and software.
+        </p>
         <p>
           I started my career as a field service engineer, providing technical solutions for machines like POS systems and data servers. Later, I moved into an IT engineer role in an office, where I gained experience in management and technical tasks.
         </p>
         <p>
-          I then transitioned to a junior web developer role, where I deepened my knowledge on core web technologies such as HTML, CSS, and JavaScript. On the front end, I worked with React to build dynamic user interfaces and learned how to create efficient, maintainable code using component-based architecture. I am eager to expand my React knowledge, particularly with advanced features like state management, hooks, and lifecycle methods.
+          I then transitioned to a junior web developer role, where I deepened my knowledge on core web technologies such as HTML, CSS, and JavaScript. On the front end, I worked with Bootstrap for the previous systems I developed and maintained. Currently, I am working with React to build dynamic user interfaces and have learned how to create efficient, maintainable code using component-based architecture. I am eager to expand my React knowledge, particularly with advanced features like state management, hooks, and lifecycle methods.
         </p>
         <p>
           On the back end, I have experience with PHP and databases like MySQL and Oracle. I’ve built full-stack applications, handling both client-side and server-side tasks, which has helped me understand how to structure applications and integrate front-end and back-end components seamlessly.
         </p>
-        <p>I’m always looking for new challenges and opportunities to grow as a developer, and I’m excited to continue building applications that make a meaningful impact.</p>
+        <p>
+          I’m always looking for new challenges and opportunities to grow as a developer, and I’m excited to continue building applications that make a meaningful impact.
+        </p>
       </div>
     </>
   )
